fc694a3ab6387967f76483a0f2e86dcdbd68a033
devmomit <dev@momit.co.kr>
2022-09-30 17:23:05 +0900

    라이브러리 수정

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import React from "react";
-import { DndProvider, useDrag, useDrop } from "react-dnd";
+import Example from "./Example";
+import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 function App() {
-  const [{ isDragging }, dragRef, previewRef] = useDrag(() => ({
-    type: "Nemo",
-    collect: (monitor) => ({
-      isDragging: monitor.isDragging(),
-    }),
-  }));
-
   return (
     <DndProvider backend={HTML5Backend}>
-      <div ref={dragRef} title="다른 카드 옆으로 드래그해서 위치를 변경합니다.">
-        드래그
-      </div>
+      <Example />
     </DndProvider>
   );
 }
